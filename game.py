cde03ff86029021152e8d6fefbcb51c485622ef0
paripatel <paripatel181193@gmail.com>
2026-05-20 15:41:18 +0200

    change  jump and second

--- game.py
+++ game.py
@@ -13,13 +13,13 @@ HEIGHT = 600
 
 #game phtsics & settings - Groung & Gravity
 GROUND = 464 # y-position hero stand here (height 600)
-GRAVITY = 700 #Pulls the hero downward after jumping
+GRAVITY = 200 #Pulls the hero downward after jumping
 
 NUMBER_OF_BACKGROUND = 2  #2 bg
 GAME_SPEED = 100 # speed of game movement
 
 JUMP_SPEED = 200 #upward speed when hero jump
-JUMP_HEIGHT = 300 #height of the hero's jump
+JUMP_HEIGHT = 360 #height of the hero's jump
 
 game_state = "intro"
 start_time = 0
@@ -34,7 +34,7 @@ KNIFE_APPARTION = (5, 9) # enemy boxes appear every 2 to 5 second rendomly
 next_knife_time = randint(KNIFE_APPARTION[0], KNIFE_APPARTION[1])  #Chooses random starting spawn time
 knifes = []
 
-BOX_APPARTION = (2, 5)
+BOX_APPARTION = (6, 10)
 next_box_time = randint(BOX_APPARTION[0], BOX_APPARTION[1])
 boxes = []
 
