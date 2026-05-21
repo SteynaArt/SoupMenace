1e4d68f7c967987f4b29bcf560f0515b63d00c8b
paripatel <paripatel181193@gmail.com>
2026-05-21 09:14:34 +0200

    second and pot

--- game.py
+++ game.py
@@ -228,7 +228,7 @@ def update(dt):
             score += 1
     # ---------------- SPAWN BOX ----------------
     if next_box_time <= 0:
-        box = Actor("box", anchor=('left', 'bottom'))
+        box = Actor("pot", anchor=('left', 'bottom'))
         box.pos = WIDTH, GROUND
         boxes.append(box)
         next_box_time = randint(BOX_APPARTION[0], BOX_APPARTION[1])
